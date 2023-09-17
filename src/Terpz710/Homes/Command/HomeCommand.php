db6f8f30abe76b1855a83d15d8940c95b9487493
Spegatti <145265002+Spegatti@users.noreply.github.com>
2023-09-17 06:05:27 -0700

    Update HomeCommand.php

--- src/Terpz710/Homes/Command/HomeCommand.php
+++ src/Terpz710/Homes/Command/HomeCommand.php
@@ -18,7 +18,7 @@ class HomeCommand extends Command {
     public function __construct(string $dataFolder) {
         parent::__construct("home", "Teleport to your home location");
         $this->setPermission("homes.home");
-        $this->dataFolder = $plugin->getDataFolder();
+        $this->dataFolder = getDataFolder();
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
